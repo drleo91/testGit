a309fc6b64a433d2efed8a8b24eb3d6eb599e123
drleo91 <drleo91@users.noreply.github.com>
2015-06-25 11:20:55 -0700

    Update Test.java
    add prinln

--- test/src/Test.java
+++ test/src/Test.java
@@ -8,6 +8,7 @@ public class Test {
 
     public void eune(){
       System.out.printf("euuuuuuuuuuuuuune") ;
+      System.out.println();
     }
     public static void main(String[] args) {
         System.out.println("here");
